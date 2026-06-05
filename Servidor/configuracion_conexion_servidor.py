146935075aa981baaa8184846751e57512fb779e
Daniel Molinas <rodanielmolinas@gmail.com>
2026-06-05 18:10:49 -0300

    Agrega configuracion de conexion del servidor

---
+++ Servidor/configuracion_conexion_servidor.py
@@ -0,0 +1,5 @@
+#conexion cliente y servidor
+HOST = "127.0.0.1" #dirección IP a la que se conectará "esta misma computadora"
+PORT = 3000 # puerta número 3000 para comunicarse
+BUFFER = 1024 # bytes que reciben o envían por vez osea lleva y trae hasta 1024 bytes de paquetes
+DELAY = 3 # retraso de tiempo que va esperar
