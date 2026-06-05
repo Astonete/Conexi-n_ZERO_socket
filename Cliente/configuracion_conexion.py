aef70300d15fe852c1138b7e63f3f8abefdd7f86
Daniel Molinas <rodanielmolinas@gmail.com>
2026-06-05 17:51:03 -0300

    se redujo el tiempo de espera a 3

--- Cliente/configuracion_conexion.py
+++ Cliente/configuracion_conexion.py
@@ -2,4 +2,4 @@
 HOST = "127.0.0.1" #dirección IP a la que se conectará "esta misma computadora"
 PORT = 3000 # puerta número 3000 para comunicarse
 BUFFER = 1024 # bytes que reciben o envían por vez osea lleva y trae hasta 1024 bytes de paquetes
-DELAY = 7 # retraso de tiempo que va esperar
+DELAY = 3 # retraso de tiempo que va esperar
