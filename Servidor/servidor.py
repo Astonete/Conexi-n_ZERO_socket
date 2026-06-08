7c0d5a2a0fb91c9f5291c5e65bc91fcb181b56f5
Daniel Molinas <rodanielmolinas@gmail.com>
2026-06-08 20:31:36 -0300

    Actualiza Servidor/servidor.py para manejar la configuración del socket y registrar errores al iniciar

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -2,17 +2,38 @@ import socket
 # por que el server no tiene usa o no tiene Delay?
 from configuracion_conexion_servidor import HOST,PORT
 from gestor_clientes import aceptar_clientes
+from datetime import datetime
+
+def hora_actual():
+    return datetime.now().strftime("fecha %d/%m/%Y hr %H:%M:%S")
 
 def iniciar_servidor():
-    # crea un socket TCP/IP y lo enlaza a la direccion y puerto especifico y luego empieza a escuchar las conexiones entrantes
+    # Crea un socket TCP/IP, lo vincula a una dirección y puerto, y escucha conexiones entrantes.
     try:
-        servidor_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM) # crea un socket usuando ip4 y tcp
-        servidor_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)# permite reutilizar la direccion del socket para evitar el error "Address already in use" al reiniciar el servidor
-        servidor_socket.bind((HOST,PORT))# enlaza el socket a la direccion y puerto especifico para que el servidor pueda escuchar las conexiones entrantes en esa direccion y puerto
-        servidor_socket.listen()# pone el socket en modo de escucha para aceptar conexiones entrantes, el numero 5 es el tamaño maximo de la cola de conexiones pendientes
-        servidor_socket.setblocking(False)# si no hay nada lanza expecion(no bloqueo)
-        print(f"---Servidor Escuchando (soy todo oidos 👂)---\n En IP: {HOST}\n Puerto: {PORT}")
-        aceptar_clientes(servidor_socket)# inicia el bucle principal del servidor para aceptar y manejar clientes
+        # 1. crea un socket usando IPv4 y TCP
+        servidor_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # 2. Permite reutilizar la dirección del socket al reiniciar el servidor.
+        #    Evita el error "Address already in use"
+        servidor_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # 3. Vincula el socket a la IP y puerto concretos para que el servidor pueda escuchar ahí las conexiones que lleguen
+        servidor_socket.bind((HOST, PORT))
+
+        # 4. Activa la escucha de conexiones; 5 es el máximo de conexiones en espera.
+        servidor_socket.listen()
+
+        # 5. Si no hay nada, no bloquea
+        servidor_socket.setblocking(False)
+
+        print(
+            f"---Servidor Escuchando (soy todo oídos 👂)---\n"
+            f" En IP: {HOST}\n"
+            f" Puerto: {PORT}\n"
+            f"{hora_actual()}hrs."
+        )
+
+        aceptar_clientes(servidor_socket)  # inicia el bucle principal del servidor para aceptar y manejar clientes
     except OSError as e:
-        print(f"Error al Configurar el servidor: {e}")
-        exit(1)# el servidor no pudo iniciar por un error de sistema, como puerto ocupado o falta de permisos
+        print(f"Error al configurar el servidor: {e} {hora_actual()}hrs.")
+        exit(1)  # el servidor no pudo iniciar por un error de sistema, como puerto ocupado o falta de permisos
